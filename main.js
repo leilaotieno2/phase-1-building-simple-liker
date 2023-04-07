6c6838174ab3454db913b8614ad591a55c8162f4
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-07 10:32:48 +0300

    first commit

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+const errorModal = document.getElementById('modal');
+const likeButtons = document.querySelectorAll('.like');
 
+errorModal.classList.add('hidden');
 
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
+likeButtons.forEach((button) => {
+  button.addEventListener('click', async () => {
+    try {
+      if (button.children[0].textContent === EMPTY_HEART) {
+        button.children[0].textContent = FULL_HEART;
+        button.classList.add('activated-heart');
       } else {
-        resolve("Pretend remote server notified of action!");
+        button.children[0].textContent = EMPTY_HEART;
+        button.classList.remove('activated-heart');
+      }
+
+      await mimicServerCall();
+    } catch (error) {
+      // Handle the error without showing the error modal
+      console.error(error);
+    }
+  });
+});
+
+function mimicServerCall(url = 'http://mimicServer.example.com', config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
+      if (isRandomFailure) {
+        reject('Random server error. Try again.');
+      } else {
+        resolve('Pretend remote server notified of action!');
       }
     }, 300);
   });
